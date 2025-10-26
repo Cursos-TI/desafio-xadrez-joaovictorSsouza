f5aa0a6a441809a2bbfb7863dea1acea39c17f74
joaovictorSsouza <joaovictor.souzacontato@gmail.com>
2025-10-26 11:43:43 +0000

    adicionado recursividade e loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,73 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 // ALUNO: João Victor Azevedo de Souza.
 
+
+// Funções para Movimentos das peças.
+
+void torre(int casas){
+    /*Recebendo como parametro casas que seria
+      a quantidade de vezes que vamos mexer a peça 
+      e criar as condições.*/
+
+      if (casas > 0){
+        printf("             direita...           \n");
+        torre(casas - 1);
+      }
+}
+
+void bispo(int casas){
+
+    /*Recebendo como parametro casas que seria
+      a quantidade de vezes que vamos mexer a peça 
+      e criar as condições.*/
+
+      for (int horizontal = 1; horizontal <= casas; horizontal++){
+        for (int vertical = 0; vertical < 1; vertical++){
+            printf("             direita...           \n");
+            printf("             cima...             \n");
+        }
+      }
+}
+
+void rainha(int casas){
+    /*Recebendo como parametro casas que seria
+      a quantidade de vezes que vamos mexer a peça 
+      e criar as condições.*/
+
+      if (casas > 0){
+        rainha(casas - 1);
+        printf("             esquerda...           \n");
+      }
+
+
+
+
+}
+
+void cavalo(){
+    /*Recebendo como parametro casas que seria
+      a quantidade de vezes que vamos mexer a peça 
+      e criar as condições.*/
+
+      for (int vertical = 0; vertical < 3; vertical++){
+        for (int horizontal = 0; horizontal < 3; horizontal++){
+            if (vertical <2 && horizontal == 0){
+                printf("             cima...             \n");
+            }else if (vertical == 2 && horizontal == 1){
+                printf("             direita...           \n");
+            }
+            
+        }
+        
+      }
+
+
+
+
+
+
+}
+
 int main() {
     const int move_torre = 5, move_bispo = 5, move_queen = 8;
     int passo_bispo = 0, passo_queen = 0;
@@ -12,73 +79,48 @@ int main() {
 
     // Implementação de Movimentação da Torre
     // Sistema: Utilização do For para mover a Torre.
-    /* For fazendo com que C seja acrescentado +1
-       a cada repetição até chegar no valor final 
-       da mivimenta ção da torre (move_torre).*/
+   // Movimento da peça a partir de uma função.
 
     printf("----------------------------------\n");
     printf("         MOVENDO A TORRE!         \n");
     printf("\n");
 
-    for (int c = 0; c < move_torre; c++){ 
-        printf("             direita...           \n");
-    }
+   torre(move_torre);
+   printf("\n");
     
     // Implementação de Movimentação do Bispo
     // Sistema: Utilização do While para mover o Bispo.
-    /* While fazendo com que passo_bispo seja acrescentado +1
-       a cada repetição enquanto o prorprio for menor que o 
-       move_bispo.*/
+    /* Função fazendo o loop aninhado. */
 
     printf("----------------------------------\n");
     printf("          MOVENDO A BISPO!        \n");
     printf("\n");
 
-    while (passo_bispo < move_bispo)
-    {
-        printf("             direita...           \n");
-        printf("              cima...             \n");
-        passo_bispo += 1; 
-    }
+    bispo(move_bispo);
     
     // Implementação de Movimentação da Rainha
     // Sistema: Utilização do Do...While para mover a Rainha.
-    /* Do...While fazendo com que PASSO_QUEEN seja acrescentado +1
-       na primeira repetição e só irá movimentar novamente enquanto
-       passo_queen for menor que move_queen.*/
+    // Movimento da peça a partir de uma função.
 
     printf("----------------------------------\n");
     printf("         MOVENDO A RAINHA!        \n");
     printf("\n");
 
-    do
-    {
-        printf("             esquerda...           \n");
-        passo_queen += 1;
-    } while (passo_queen < move_queen);
+    rainha(move_queen);
+
 
     // Implementação de Movimentação da Cavalo
     // Sistema: Utilização For e while para mover o cavalo.
-    /* For travado até dois por que o cavalo só pode se mover 2 vezes na mexida 
-       e na segunda mexida o while trava para ocorrer apenas 1 vez.*/
+    // Movimento da peça a partir de uma função./
 
     printf("----------------------------------\n");
     printf("         MOVENDO O CAVALO!        \n");
     printf("\n");
 
-    for (int c = 0; c < 2;){
-        printf("             baixo...             \n");
-        c += 1;
-        while (c == 2)
-        {
-            printf("             esquerda...           \n");
-            break;
-        }
+   cavalo();
         
-    }
-
-
     printf("----------------------------------\n");
 
     return 0;
 }
+
